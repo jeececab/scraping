adc22e37cf247a6a01e8b5ef25c83254e82ef5de
Jean-Christophe Seguin Cabana <jean-christophe.cabana@altitude-sports.com>
2018-12-29 10:42:34 -0500

    adds sporting-life-href

---
+++ nightwatch/config/nightwatch.conf.js
@@ -0,0 +1,36 @@
+const seleniumServer = require('selenium-server');
+const chromedriver = require('chromedriver');
+
+nightwatch_config = {
+    src_folders: ['./nightwatch/tests'],
+    output_folder: './nightwatch/reports',
+    selenium: {
+        start_process: true,
+        server_path: seleniumServer.path,
+        log_path: false,
+        host: '127.0.0.1',
+        port: 4444,
+        cli_args: {
+            'webdriver.chrome.driver': chromedriver.path
+        },
+    },
+    test_settings: {
+        default: {
+            selenium_port: 4444,
+            selenium_host: 'localhost',
+            silent: true,
+            screenshots: {
+                enabled: false,
+                path: '',
+            },
+            desiredCapabilities: {
+                browserName: 'chrome',
+                chromeOptions: {
+                  args: ["headless"]
+                }
+            },
+        }
+    }
+};
+
+module.exports = nightwatch_config;
