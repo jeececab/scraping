851713b24d14ddb0fdae2295317fc9f14cd0a3dc
Jean-Christophe Seguin Cabana <jean-christophe.cabana@altitude-sports.com>
2018-12-30 11:12:15 -0500

    more dynamism + readme update

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # scraping
+
+To use
+
+1 - In the sporting-life-href.js file, enter your database link in "this.database"
+2 - In the sporting-life-href.js file, enter the brands names (in the Sporting Chek format) in the array "this.brands"
+3 - Run: "npm run nightwatch" in the command line
+
+Note: not all brands have a "shop-all" url on Sport Chek, only the ones with a lot of articles. So entering a non valid brand will break the code.
--- nightwatch/tests/sporting-life-href.js
+++ nightwatch/tests/sporting-life-href.js
@@ -1,59 +1,54 @@
 const axios = require('axios');
 
 module.exports = {
-  beforeEach: client => {
-    client.deleteCookies();
+  before: client => {
+    this.database = 'https://scrapping-jc.firebaseio.com'
+    this.brands = ['the-north-face', 'arcteryx', 'mountain-hardwear'];
+    this.brands.forEach(brand => {
+      axios.delete(
+        `${this.database}/sporting-life-${brand}.json`
+      );
+    });
   },
 
   after: client => {
     client.end();
   },
 
-  "extracts TNF products's href from Sportchek brand page": client => {
-    getHrefs(
-      client,
-      'https://www.sportchek.ca/brands/the-north-face/shop-all.html',
-      'tnf'
-    );
-  },
+  "extracts the products's href from Sportchek selected brand pages": client => {
+    this.brands.forEach(brand => {
+      client
+        .url(`https://www.sportchek.ca/brands/${brand}/shop-all.html`)
+        .waitForElementVisible('.results-quantity')
+        .pause(1000)
+        .getText('.results-quantity', div => {
+          const nbOfItems = parseInt(div.value.split(' ')[0]);
+          const itemsGroup = nbOfItems / 16;
+          console.log(
+            `Hrefs extaction for ${brand} is in progress, please wait...`
+          );
+          for (let i = 1; i <= itemsGroup + 2; i++) {
+            client.getLocationInView('footer').pause(500);
+          }
 
-  "extracts Arcteryx products's href from Sportchek brand page": client => {
-    getHrefs(
-      client,
-      'https://www.sportchek.ca/brands/arcteryx/shop-all.html',
-      'arcteryx'
-    );
+          client
+            .waitForElementVisible(`[data-id="${nbOfItems - 8}"]`)
+            .elements(
+              'css selector',
+              '.product-title .product-grid__link',
+              result => {
+                result.value.forEach(el => {
+                  client.elementIdAttribute(el.ELEMENT, 'href', el => {
+                    axios.post(
+                      `${this.database}/sporting-life-${brand}.json`,
+                      { href: el.value }
+                    );
+                  });
+                });
+                console.log(`Hrefs extaction for ${brand} is done`);
+              }
+            );
+        });
+    });
   }
 };
-
-function getHrefs(client, url, brand) {
-  client
-    .url(url)
-    .waitForElementVisible('.results-quantity')
-    .pause(1000)
-    .getText('.results-quantity', div => {
-      const nbOfItems = parseInt(div.value.split(' ')[0]);
-      const itemsGroup = nbOfItems / 16;
-
-      for (let i = 1; i <= itemsGroup; i++) {
-        client.getLocationInView('footer').pause(500);
-      }
-
-      client
-        .waitForElementVisible(`[data-id="${nbOfItems - 8}"]`)
-        .elements(
-          'css selector',
-          '.product-title .product-grid__link',
-          result => {
-            result.value.forEach(el => {
-              client.elementIdAttribute(el.ELEMENT, 'href', el => {
-                axios.post(
-                  `https://scrapping-jc.firebaseio.com/sporting-life-${brand}.json`,
-                  { href: el.value }
-                );
-              });
-            });
-          }
-        );
-    });
-}
